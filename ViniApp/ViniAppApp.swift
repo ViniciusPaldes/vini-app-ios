99591d770fce22d62598fc2aee9fb5e397fb4fed
Vinicius Paldês <viniciuspaldes@Viniciuss-MacBook-Pro.local>
2024-01-25 20:30:27 -0300

    Project created on XCode

---
+++ ViniApp/ViniAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViniAppApp.swift
+//  ViniApp
+//
+//  Created by Vinicius Paldês on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ViniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
